9dff565c82428dac559bd81555431d0b97bde821
Zims <zims@inbox.lv>
2021-09-02 13:12:19 +0300

    dynamic month urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
-# Create your views here.
+
+monthly_challenges = {
+    "january": "jan",
+    "february": "feb",
+    "march": "mar",
+    "april": "apr",
+    "may": "may",
+    "june": "jun",
+    "july": "jul",
+    "august": "aug",
+    "september": "sept",
+    "october": "oct",
+    "november": "nov",
+    "december": "dec",
+}
+
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Month not found")
+    redirect_month = months[month-1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+    except KeyError:
+        return HttpResponseNotFound("Month not found")
+
+    return HttpResponse(challenge_text*20)
