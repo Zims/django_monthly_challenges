1516215a8aa9cb3448136242861636daffbf0c79
Zims <zims@inbox.lv>
2021-09-07 16:46:39 +0300

    base.html template

--- challenges/views.py
+++ challenges/views.py
@@ -15,18 +15,16 @@ monthly_challenges = {
     "september": "september",
     "october": "october",
     "november": "november",
-    "december": "december",
+    "december": None,
 }
 
 def index(request):
-    list_items = ""    
     months = list(monthly_challenges.keys())
-    for month in months:
-        month_path = reverse("month-challenge", args=[month])
-        capitlized_month = month.capitalize()
-        list_items += f"<li><a href=\"{month_path}\">{capitlized_month}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, 
+                "challenges/index.html", 
+                {"months": months}
+                )
+
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
